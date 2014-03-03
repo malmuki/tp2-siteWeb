683cdbc79ca6a0f567baadf21a017296100423fa
leguidonviolon <alex-s-cape@hotmail.com>
2014-03-03 11:26:30 -0500

    v0.4
    Avec les fichiers texte.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,24 +1,50 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
         Session["loguer"] = false;
     }
 
     protected void btnConnect_Click(object sender, EventArgs e)
     {
-        if (txbID.Text == "12345" && txtbPass.Text == "allo")
-        {
-            Session["id"] = "12345";
-            Session["loguer"] = true;
-            Response.Redirect("FormReservation.aspx");
-        }
-        else
-        {
-            
-        }
+        List<string> listUsernames = new List<string>();
+        List<string> listPasswords = new List<string>();
+
+            TextReader txtReader = new StreamReader("data.txt");
+
+            for (int i = 0; i < File.ReadAllLines("data.txt").Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    listUsernames.Add(txtReader.ReadLine());
+                }
+                else
+                {
+                    listPasswords.Add(txtReader.ReadLine());
+                }
+            }
+
+            string[] tabUsernames = listUsernames.ToArray();
+            string[] tabPasswords = listPasswords.ToArray();
+
+            for (int i = 0; i < tabUsernames.Length; i++)
+            {
+                if (txbID.Text == tabUsernames[i] && txtbPass.Text == tabPasswords[i])
+                {
+                    Session["id"] = tabUsernames[i];
+                    Session["loguer"] = true;
+                    Response.Redirect("FormReservation.aspx");
+                }
+                else
+                {
+
+                }
+            }      
     }
 
     protected void btnInscrire_Click(object sender, EventArgs e)

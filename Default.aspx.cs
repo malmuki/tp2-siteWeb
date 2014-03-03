c8b3e86759bf4ac7c15719a7b4c723225d24c9ba
leguidonviolon <alex-s-cape@hotmail.com>
2014-03-03 11:42:17 -0500

    criss
    criss

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -12,6 +12,7 @@ public partial class _Default : System.Web.UI.Page
 
     protected void btnConnect_Click(object sender, EventArgs e)
     {
+<<<<<<< HEAD
         List<string> listUsernames = new List<string>();
         List<string> listPasswords = new List<string>();
 
@@ -45,6 +46,18 @@ public partial class _Default : System.Web.UI.Page
 
                 }
             }      
+=======
+        if (txbID.Text == "12345" && txtbPass.Text == "allo")
+        {
+            Session["id"] = "12345";
+            Session["loguer"] = true;
+            Response.Redirect("FormReservation.aspx");
+        }
+        else
+        {
+            lblError.Visible = true;
+        }
+>>>>>>> 60f2980cbe8731d5724e999547473448df03f0f5
     }
 
     protected void btnInscrire_Click(object sender, EventArgs e)
